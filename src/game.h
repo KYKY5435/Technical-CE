847f7c6ae89d75573e2e7d5483ff24e54bac97f5
KYKY5435 <madeinnyc2009@gmail.com>
2026-09-22 19:40:06 -0400

    fix 4

--- src/game.h
+++ src/game.h
@@ -10,5 +10,5 @@ void tile_selection_menu(uint8_t &current_selection, const tinystl::unordered_ma
 
 extern uint24_t seed, x, y;
 
-extern constexpr uint24_t BASE_X = GFX_LCD_WIDTH;
-extern constexpr uint8_t BASE_Y  = GFX_LCD_HEIGHT;
+extern const uint24_t BASE_X;
+extern const uint8_t BASE_Y;
